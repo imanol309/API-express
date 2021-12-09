3b4bc8839b07b80d4218a016dbcdcd37efc71312
imanol309 <imanolguzman309@gmail.com>
2021-12-09 11:46:10 -0400

    modifying the index.js view

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 const mysql =  require('mysql') 
 const app =  express()
 app.use(cors())
+// settings
 app.set('port', process.env.PORT || 4000)
+
+//credentials
 
 const credentials={
     host: 'localhost',
@@ -12,10 +15,12 @@ const credentials={
     database: 'init'
 }
 
+//router the serve, main router
 app.get('/', (req, res) => {
     res.send('hola desde tu datos')
 })
 
+//routes to view data
 app.get('/datos', (req, res) => {
     var connection= mysql.createConnection(credentials)
     connection.query('SELECT * FROM usuarios', (error, result)=> {
@@ -27,4 +32,16 @@ app.get('/datos', (req, res) => {
     })
 })
 
+// app.port('/enviar', (req, res) => {
+//     connection.query('INSERT INTO usuarios',[req.body], (error, result)=> {
+//         if(error) {
+//             res.status(500).send(error)
+//         } else {
+//             res.status(200).send(result)
+//         }
+//     })
+// })
+
+
+// starting the serve 
 app.listen(app.get('port'), () => console.log(`Servidor abierto con exito en el puerto ${app.get('port')}`))
